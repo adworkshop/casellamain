c9387d0b539c738f293a84d7187617b7137ed386
wolffereast <stephenhwolff@gmail.com>
2016-12-15 14:28:49 -0500

    Added type specific breadcrumb overrides

--- docroot/modules/custom/adw_breadcrumbs/src/BreadcrumbBuilder.php
+++ docroot/modules/custom/adw_breadcrumbs/src/BreadcrumbBuilder.php
@@ -50,7 +50,44 @@ class BreadcrumbBuilder extends PathBasedBreadcrumbBuilder {
     $breadcrumbs = new Breadcrumb();
     $position = 1;
     if (count($menuLink)) {
-      $this->extractBreadcrumbs($breadcrumbs, $position, $menuLink, $menuLinkManager, \Drupal::menuTree());
+      $this->extractBreadcrumbsDefault($breadcrumbs, $position, $menuLink, $menuLinkManager, \Drupal::menuTree());
+    }
+    else {
+      // Not in the nav, check if this is one of the special case nodes.
+      $node = \Drupal::routeMatch()->getParameter('node');
+      if ($node) {
+        $type = $node->getType();
+
+        $baseNid = FALSE;
+        if ('job' == $type) {
+          $baseNid = '696';
+        }
+        elseif ('information_post' == $type) {
+          $baseNid = '36';
+        }
+        elseif ('product' == $type) {
+          // See if we can get a product category.
+          $cat = $node->get('field_product_category');
+          if ($cat) {
+            $cat = $cat->getValue();
+            if ($cat) {
+              $baseNid = $cat[0]['target_id'];
+            }
+          }
+          if (!$baseNid) {
+            $baseNid = '26';
+          }
+        }
+
+        if ($baseNid) {
+          $trailIds = $menuLinkManager->loadLinksByRoute('entity.node.canonical', array('node' => $baseNid));
+          // Pull the IDs.
+          $trailIds = array_keys($trailIds);
+          // Get the full list. Conveniently comes witht he current ID included.
+          $trailIds = $menuLinkManager->getParentIds($trailIds[0]);
+          $this->extractBreadcrumbsFromTrail($breadcrumbs, $position, $trailIds, $menuLinkManager);
+        }
+      }
     }
 
     // Add the current page title.
@@ -100,18 +137,30 @@ class BreadcrumbBuilder extends PathBasedBreadcrumbBuilder {
    * @param $menuLinkManager
    * @param $menuTree
    */
-  private function extractBreadcrumbs(&$breadcrumbs, &$position, $menuLink, $menuLinkManager, $menuTree) {
+  private function extractBreadcrumbsDefault(&$breadcrumbs, &$position, $menuLink, $menuLinkManager, $menuTree) {
     $menuLink = array_shift($menuLink);
     $currentPluginId = $menuLink->getPluginId();
 
     // Build the typical default set of menu tree parameters.
     $menu_name = $menuLink->getMenuName();
     $parameters = $menuTree->getCurrentRouteMenuTreeParameters($menu_name);
+
     $trailIds = $parameters->activeTrail;
 
+    $this->extractBreadcrumbsFromTrail($breadcrumbs, $position, $trailIds, $menuLinkManager, $currentPluginId);
+  }
+
+  /**
+   * @param $breadcrumbs
+   * @param $position
+   * @param $trailIds
+   * @param $menuLinkManager
+   * @param string $currentPluginId
+   */
+  private function extractBreadcrumbsFromTrail(&$breadcrumbs, &$position, $trailIds, $menuLinkManager, $currentPluginId = '') {
     $db = \Drupal\Core\Database\Database::getConnection();
 
-    foreach (array_reverse($trailIds) as $key => $value) {
+    foreach (array_reverse($trailIds) as $value) {
       if ($value && $value !== $currentPluginId) {
         $instance = $menuLinkManager->createInstance($value);
         $urlObject = $instance->getUrlObject();
