55aa2ce1eab7e8c76aa2856e05e287a1b3576fc9
Stephen Wolff <stephenhwolff@gmail.com>
2017-02-15 09:14:32 -0500

    Added test curl file

---
+++ docroot/test.php
@@ -0,0 +1,20 @@
+<?php
+
+// create curl resource
+$ch = curl_init();
+
+// set url
+curl_setopt($ch, CURLOPT_URL, "http://8.225.179.58/export/locations/nondrop.xml");
+
+//return the transfer as a string
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+curl_setopt($ch, CURLOPT_PORT, 8080);
+
+// $output contains the output string
+$output = curl_exec($ch);
+
+// close curl resource to free up system resources
+curl_close($ch);
+
+print $output;
