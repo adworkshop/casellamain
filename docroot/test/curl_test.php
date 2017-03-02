455e59a2c13c59093697284e7597caea0f3ed89e
wolffereast <stephenhwolff@gmail.com>
2017-03-02 11:09:01 -0500

    Added x forwarded for debug and curlopt interface setting

--- docroot/test/curl_test.php
+++ docroot/test/curl_test.php
@@ -4,6 +4,10 @@ set_time_limit(300);
 print 'Current IP as reported by SERVER_ADDR: ' . $_SERVER['SERVER_ADDR'] . "\n<br />";
 $localIP = getHostByName(getHostName());
 print 'Current IP as reported by getHostByName(getHostName()): ' . $localIP . "\n<br />";
+$x_forwarded = isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : 'HTTP_X_FORWARDED_FOR not set';
+print 'HTTP_X_FORWARDED_FOR: ' . $x_forwarded . "\n<br />";
+
+
 print 'Attempting to curl: "http://8.225.179.58/export/locations/nondrop.xml"' . "\n<br />";
 print 'using curl_setopt to set the CURLOPT_PORT to 8080' . "\n<br />";
 
@@ -12,6 +16,10 @@ $ch = curl_init();
 
 // set url
 curl_setopt($ch, CURLOPT_URL, "http://8.225.179.58/export/locations/nondrop.xml");
+
+// Try setting the load balancer to use.
+print 'setting CURLOPT_INTERFACE to 54.82.179.35' . "\n<br />";
+curl_setopt($curlh, CURLOPT_INTERFACE, "54.82.179.35");
 
 //return the transfer as a string
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
